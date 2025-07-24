14a13ca7ee1c6864c846b832c8847ca5c02de20e
Nikita S <nikitasawant5193@gmail.com>
2025-07-24 12:39:41 +0530

    23/7/25

---
+++ Operators.js
@@ -0,0 +1,34 @@
+// +, -, *, /, %, ++, --
+let a=5;
+let b=101;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(b/a);
+console.log(b%a);
+console.log(a++);
+console.log(b++);
+console.log(++a);
+console.log(++b);
+
+//Comparison: == (checks value), === (checks value + type), !=, !==, <, >, <=, >=
+let c="5";
+let d=5;
+console.log(a==b);
+console.log(a==c);
+console.log(a==d);
+console.log(a===b);
+console.log(a===c);
+console.log(a===d);
+console.log(a<b);
+console.log(a>=b);
+console.log(a!==b);
+
+//Logical: && (AND), || (OR), ! (NOT)
+let num1=6;
+let num2=7;
+console.log(num1!=num2 && num1<num2);
+console.log(num1==num2 || num1<num2);
+
+
+

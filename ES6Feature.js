ef969a33fa43772bc5c4828a7cf03e4bd80d56fd
Nikita S <nikitasawant5193@gmail.com>
2025-07-26 16:15:54 +0530

    New features implemented in 6+ versions of js

---
+++ ES6Feature.js
@@ -0,0 +1,12 @@
+//ES6(ECMAScript 2015) features not supported by this browser use 6+ v..Modern javascript improvement-->let & const
+let [a,b]=[5,6]
+console.log(b)
+//Template Literals:
+ console.log(Hello, ${a}!);
+//Destructuring:
+ let [b, c] = [10, 20];
+console.log(a); // 10
+//Spread/Rest Operators:
+ let arr1 = [1, 2, 3];
+let arr2 = [...arr1, 4, 5];
+

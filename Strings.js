97bf98e97d2dbc79c01558d856c7dce2d6ecd10b
Nikita S <nikitasawant5193@gmail.com>
2025-07-26 15:08:38 +0530

    String

---
+++ Strings.js
@@ -0,0 +1,7 @@
+let str="hello world"
+let f=[1,2,3]
+console.log(str.split(" ")); // Converts string to array
+console.log(f.join()) //Converts array to string
+console.log(str.replace(" ","@")) // Replaces part of a string
+console.log(str.substring(3)) // Extracts part of a string
+console.log(str.trim())
